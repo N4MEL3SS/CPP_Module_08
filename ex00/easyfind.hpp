c952f32efcb7e5b2781cf14af5f5061b34af737c
celadia <celadia@21-school.ru>
2022-09-28 01:39:23 +0300

    start ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,6 +1,19 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
 
+template <typename T>
+typename T::const_iterator easyFind(const T &container, const int toFind)
+{
+	typename T::iterator iter;
+
+	iter = std::find(container.begin(), container.end(), toFind);
+
+	if (iter == container.end())
+		throw (std::exception());
+	return (iter);
+}
 
 #endif //EASYFIND_HPP
